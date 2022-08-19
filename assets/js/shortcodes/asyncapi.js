732f4aa2575871e189ff7202f72f953caa6f8d99
Jordan GAZEAU <jordan.gazeau@gmail.com>
2022-08-19 23:44:00 +0200

    fix: add loading for asyncapi shortcode and manage dependencies with bmyc

---
+++ assets/js/shortcodes/asyncapi.js
@@ -0,0 +1,20 @@
+import {
+  getFirstAncestorByClass
+} from '../theme/modules/helpers.min.js'
+
+// VARS //
+// MAIN //
+document.addEventListener('DOMContentLoaded', function() {
+  renderAllAsyncapi();
+});
+function renderAllAsyncapi() {
+  let diva = document.getElementsByTagName('asyncapi-component');
+  for (let i = 0; i < diva.length; i++) {
+    renderAsyncapi(diva[i]);
+  }
+}
+function renderAsyncapi(ac) {
+  setTimeout(function() {
+    getFirstAncestorByClass(ac, 'sc-asyncapi-wrapper').classList.toggle('is-loading', false);
+  }, 600);
+}
