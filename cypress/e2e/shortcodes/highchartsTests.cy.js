e6469d4eccf67819a91b8dbbd9eb16e30f375028
Jordan GAZEAU <jordan.gazeau@gmail.com>
2022-09-20 23:10:29 +0200

    fix: highcharts tests

--- cypress/e2e/shortcodes/highchartsTests.cy.js
+++ cypress/e2e/shortcodes/highchartsTests.cy.js
@@ -1,6 +1,9 @@
 describe('for: highcharts shortcode', () => {
   beforeEach(() => {
     cy.visit(Cypress.env('SC_PATH') + Cypress.env('SC_HIGHCHARTS_PATH'))
+    cy.on('uncaught:exception', (err, runnable) => {
+      return false
+    })
   })
   it('four highcharts should be displayed', () => {
     cy.get('#content .sc-highcharts-wrapper > .sc-highcharts-container')
